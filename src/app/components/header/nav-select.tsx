c52b6b9a3c60abb4736d367f02d92da0318101fa
Victor Merseguel <merseguel@gmail.com>
2024-12-31 00:07:20 +0000

    fix: nav-select default value

--- src/app/components/header/nav-select.tsx
+++ src/app/components/header/nav-select.tsx
@@ -1,13 +1,18 @@
 "use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const NavSelect = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <select
       name="lang"
       id="lang"
-      onChange={(e) => console.log(e.target.value)}
+      onChange={(e) => router.push(`/${e.target.value}`)}
       className="uppercase border-none ml-[-5px] bg-transparent cursor-pointer"
+      value={pathname.replace("/", "")}
     >
       <option value="en">🇬🇧 en</option>
       <option value="pt">🇵🇹 pt</option>
