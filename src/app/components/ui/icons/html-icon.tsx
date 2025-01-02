5de47d655e767dd3b2c664888276b76e168292ca
Victor Merseguel <merseguel@gmail.com>
2025-01-02 06:20:14 +0000

    feat: import ui items

---
+++ src/app/components/ui/icons/html-icon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HtmlIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.79883 0L4.20117 27L15 30L25.7988 27L28.2012 0H1.79883ZM22.9629 8.83594H10.3066L10.6348 12.2168H22.6875L21.7617 22.3594L15 24.2754L8.23828 22.3652L7.74609 17.1855H11.0742L11.291 19.8047L15 20.7891L18.709 19.8047L19.0898 15.498H7.63477L6.76172 5.45508H23.3437L22.9629 8.83594Z"
+        fill="#5E5A57"
+      />
+    </svg>
+  );
+};
+
+export default HtmlIcon;
