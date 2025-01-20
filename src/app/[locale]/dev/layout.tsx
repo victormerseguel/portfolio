221c36bccea729f7179a1eaea9ad7cb79105a059
Victor Merseguel <merseguel@gmail.com>
2025-01-20 00:04:29 +0000

    fix: vercel errors any type

--- src/app/[locale]/dev/layout.tsx
+++ src/app/[locale]/dev/layout.tsx
@@ -28,8 +28,8 @@ export default async function LocaleLayout({
 }) {
   const { locale } = await params;
 
-  if (!routing.locales.includes(locale as any)) {
-    // eslint-disable-line @typescript-eslint/no-explicit-any
+  // prettier-ignore
+  if (!routing.locales.includes(locale as any)) { // eslint-disable-line @typescript-eslint/no-explicit-any
     notFound();
   }
 
